d125a193027271aaf572aae55e702436afdb1e93
marisa <358333370@qq.com>
2018-12-06 19:53:26 +0800

    把本地工程添加到git

---
+++ SpringBootTest/springboot_jpa/src/main/java/com/marisa/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.marisa.repository;
+
+import com.marisa.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+
+    public List<User> findAll();
+
+}
